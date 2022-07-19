e736776b1db9bd26b2180f01432dacb09598f103
jinah park <tbrk8585@naver.com>
2022-07-19 14:40:33 +0900

    chore: skill styled-components change

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,73 +1,92 @@
 import React from "react";
+import styled from "styled-components";
+
+const percentColor = (percent) => {
+    if (percent <= 30) {
+        return "#F6FDA2";
+    } else if (30 < percent && percent <= 50) {
+        return "#A2EDFD";
+    } else if (50 < percent && percent <= 70) {
+        return "#ECB8FF";
+    } else if (70 < percent && percent <= 90) {
+        return "#FFB8CD";
+    } else {
+        return "#AAFDA2";
+    }
+};
 
 const Skill = ({ item }) => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                width: "100%",
-                height: "8vh",
-            }}
-        >
-            <img
-                src={item.skillImg}
-                alt="logo192"
-                style={{ width: "8.5vMin", height: "8.5vMin" }}
-            />
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-evenly",
-                }}
-            >
-                <div
-                    style={{
-                        height: "4vh",
-                        width: "30vw",
-                        border: "2px solid black",
-                        borderRadius: "20px",
-                        position: "relative",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "flex-start",
-                        paddingLeft: "0.2vw",
-                    }}
-                >
-                    <div
-                        style={{
-                            height: "3vh",
-                            width: "28vw",
-                            border: "2px dashed black",
-                            borderRadius: "18px",
-                            position: "absolute",
-                            backgroundColor: "white",
-                            lineHeight: "0.5em",
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                        }}
+        <Container>
+            <SkillImg src={item.skillImg} alt="logo192" />
+            <PercentWrapper>
+                <PercentConstruct>
+                    <Percent
+                        percent={item.percent}
+                        color={percentColor(item.percent)}
                     >
-                        <div
-                            style={{
-                                position: "absolute",
-                                right: "0",
-                                fontSize: "0.5em",
-                                paddingRight: "0.5em",
-                            }}
-                        >
+                        <Text>
                             <div>{item.percent}%</div>
-                        </div>
-                    </div>
-                </div>
+                        </Text>
+                    </Percent>
+                </PercentConstruct>
                 {/* <div style={{ width: "30vw", wordBreak: "keep-all" }}>
                     <div style={{ fontSize: "0.32em" }}>{item.comment}</div>
                 </div> 뒤집으면 코멘트 나오게? */}
-            </div>
-        </div>
+            </PercentWrapper>
+        </Container>
     );
 };
 
 export default Skill;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+    height: 8vh;
+`;
+
+const SkillImg = styled.img`
+    width: 8.5vmin;
+    height: 8.5vmin;
+`;
+
+const PercentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`;
+
+const PercentConstruct = styled.div`
+    height: 4vh;
+    width: 30vw;
+    border: 2px solid black;
+    border-radius: 20px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding-left: 0.2vw;
+`;
+
+const Percent = styled.div`
+    height: 3vh;
+    width: ${(props) => (props.percent / 100) * 30}vw;
+    border: 2px dashed black;
+    border-radius: 18px;
+    position: absolute;
+    background-color: ${(props) => props.color};
+    line-height: 0.5em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Text = styled.div`
+    position: absolute;
+    right: 0;
+    font-size: 0.5em;
+    padding-right: 0.5em;
+`;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,51 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 import Skill from "./Skill";
 import mySkills from "../mySkills.json";
 
 const Skills = () => {
     return (
         <div className="inner" style={{ fontSize: "3.5em" }}>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    height: "85vh",
-                    paddingBottom: "7vh",
-                }}
-            >
+            <Container>
                 <div>{"<Skills>"}</div>
-                <div
-                    style={{
-                        width: "100%",
-                        height: "8vh",
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                    }}
-                >
+                <InfoWrapper>
                     <img src="imgs/infoBar.png" alt="InfoBar" />
-                </div>
-                <div
-                    style={{
-                        width: "80vMin",
-                        height: "60vh",
-                        border: "2px solid black",
-                        borderRadius: "20px",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "space-evenly",
-                    }}
-                >
+                </InfoWrapper>
+                <SkillWrapper>
                     {mySkills.map((item, index) => (
                         <Skill key={`skill-${index}`} item={item} />
                     ))}
-                </div>
+                </SkillWrapper>
                 <div>{"</Skills>"}</div>
-            </div>
+            </Container>
         </div>
     );
 };
 
 export default Skills;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 85vh;
+    padding-bottom: 7vh;
+`;
+
+const InfoWrapper = styled.div`
+    width: 100%;
+    height: 8vh;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const SkillWrapper = styled.div`
+    width: 80vmin;
+    height: 60vh;
+    border: 2px solid black;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`;
