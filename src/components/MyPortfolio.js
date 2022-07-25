33798c265a040deb8645590b8f51d509aa963bbf
jinah park <tbrk8585@naver.com>
2022-07-25 18:00:18 +0900

    feat: mobile touch event add

--- src/components/MyPortfolio.js
+++ src/components/MyPortfolio.js
@@ -16,138 +16,263 @@ const MyPortfolio = () => {
     const [noDots, setNoDots] = useState(true);
 
     useEffect(() => {
-        const wheelHandler = (e) => {
-            e.preventDefault();
-            const { deltaY } = e;
+        let initialY = null;
+        // const wheelHandler = (e) => {
+        //     e.preventDefault();
+        //     const { deltaY } = e;
+        //     const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+        //     const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+        //     if (deltaY > 0) {
+        //         // 스크롤 내릴 때
+        //         if (scrollTop >= 0 && scrollTop < pageHeight) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight + DIVIDER_HEIGHT,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(2);
+        //         } else if (
+        //             scrollTop >= pageHeight &&
+        //             scrollTop < pageHeight * 2
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(false);
+        //             setScrollIndex(3);
+        //         } else if (
+        //             scrollTop >= pageHeight * 2 &&
+        //             scrollTop < pageHeight * 3
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(4);
+        //         } else if (
+        //             scrollTop >= pageHeight * 3 &&
+        //             scrollTop < pageHeight * 4
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(5);
+        //         } else {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(5);
+        //         }
+        //     } else {
+        //         // 스크롤 올릴 때
+        //         if (scrollTop >= 0 && scrollTop < pageHeight) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: 0,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(1);
+        //         } else if (
+        //             scrollTop >= pageHeight &&
+        //             scrollTop < pageHeight * 2
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: 0,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(1);
+        //         } else if (
+        //             scrollTop >= pageHeight * 2 &&
+        //             scrollTop < pageHeight * 3
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight + DIVIDER_HEIGHT,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(2);
+        //         } else if (
+        //             scrollTop >= pageHeight * 3 &&
+        //             scrollTop < pageHeight * 4
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(false);
+        //             setScrollIndex(3);
+        //         } else if (
+        //             scrollTop >= pageHeight * 4 &&
+        //             scrollTop < pageHeight * 5
+        //         ) {
+        //             outerDivRef.current.scrollTo({
+        //                 top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
+        //                 left: 0,
+        //                 behavior: "smooth",
+        //             });
+        //             setNoDots(true);
+        //             setScrollIndex(4);
+        //         }
+        //     }
+        // };
+        const initTouch = (e) => {
+            initialY = `${e.touches ? e.touches[0].clientY : e.clientY}`;
+        };
+
+        const swipeDirection = (e) => {
+            if (initialY !== null) {
+                const currentY = `${
+                    e.touches ? e.touches[0].clientY : e.clientY
+                }`;
+
+                let diffY = initialY - currentY;
+                console.log("=", initialY, currentY, diffY);
+
+                0 < diffY ? upper() : down();
+
+                initialY = null;
+            }
+        };
+
+        const upper = () => {
             const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
             const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
 
-            if (deltaY > 0) {
-                // 스크롤 내릴 때
-                if (scrollTop >= 0 && scrollTop < pageHeight) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight + DIVIDER_HEIGHT,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(2);
-                } else if (
-                    scrollTop >= pageHeight &&
-                    scrollTop < pageHeight * 2
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(false);
-                    setScrollIndex(3);
-                } else if (
-                    scrollTop >= pageHeight * 2 &&
-                    scrollTop < pageHeight * 3
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(4);
-                } else if (
-                    scrollTop >= pageHeight * 3 &&
-                    scrollTop < pageHeight * 4
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(5);
-                } else {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(5);
-                }
+            if (scrollTop >= 0 && scrollTop < pageHeight) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight + DIVIDER_HEIGHT,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(2);
+            } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(false);
+                setScrollIndex(3);
+            } else if (
+                scrollTop >= pageHeight * 2 &&
+                scrollTop < pageHeight * 3
+            ) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(4);
+            } else if (
+                scrollTop >= pageHeight * 3 &&
+                scrollTop < pageHeight * 4
+            ) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(5);
             } else {
-                // 스크롤 올릴 때
-                if (scrollTop >= 0 && scrollTop < pageHeight) {
-                    outerDivRef.current.scrollTo({
-                        top: 0,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(1);
-                } else if (
-                    scrollTop >= pageHeight &&
-                    scrollTop < pageHeight * 2
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: 0,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(1);
-                } else if (
-                    scrollTop >= pageHeight * 2 &&
-                    scrollTop < pageHeight * 3
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight + DIVIDER_HEIGHT,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(2);
-                } else if (
-                    scrollTop >= pageHeight * 3 &&
-                    scrollTop < pageHeight * 4
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(false);
-                    setScrollIndex(3);
-                } else if (
-                    scrollTop >= pageHeight * 4 &&
-                    scrollTop < pageHeight * 5
-                ) {
-                    outerDivRef.current.scrollTo({
-                        top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
-                        left: 0,
-                        behavior: "smooth",
-                    });
-                    setNoDots(true);
-                    setScrollIndex(4);
-                }
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 4 + DIVIDER_HEIGHT * 4,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(5);
             }
         };
+        const down = () => {
+            const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+            const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+            if (scrollTop >= 0 && scrollTop < pageHeight) {
+                outerDivRef.current.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(1);
+            } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                outerDivRef.current.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(1);
+            } else if (
+                scrollTop >= pageHeight * 2 &&
+                scrollTop < pageHeight * 3
+            ) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight + DIVIDER_HEIGHT,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(2);
+            } else if (
+                scrollTop >= pageHeight * 3 &&
+                scrollTop < pageHeight * 4
+            ) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(false);
+                setScrollIndex(3);
+            } else if (
+                scrollTop >= pageHeight * 4 &&
+                scrollTop < pageHeight * 5
+            ) {
+                outerDivRef.current.scrollTo({
+                    top: pageHeight * 3 + DIVIDER_HEIGHT * 3,
+                    left: 0,
+                    behavior: "smooth",
+                });
+                setNoDots(true);
+                setScrollIndex(4);
+            }
+        };
+
         const outerDivRefCurrent = outerDivRef.current;
 
-        outerDivRefCurrent.addEventListener("wheel", wheelHandler);
-        return () => {
-            outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
-        };
+        // outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+        // return () => {
+        //     outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+        // };
+        outerDivRefCurrent.addEventListener("touchstart", initTouch);
+        outerDivRefCurrent.addEventListener("touchmove", swipeDirection);
     }, []);
     return (
         <>
             {/* <Comment>
                 # UI상 해당 페이지에서 ScrollDots는 숨겨두었습니다.
             </Comment> */}
-            <div
-                ref={outerDivRef}
-                className="outer"
-                style={{ fontFamily: "East Sea Dokdo, sans-serif" }}
-            >
+            <FullPageWrapper ref={outerDivRef} className="outer">
                 {noDots && (
                     <Dots
                         scrollIndex={scrollIndex}
@@ -156,23 +281,35 @@ const MyPortfolio = () => {
                         setNoDots={setNoDots}
                     />
                 )}
-                <Intro />
+                <Intro className="yContainer" />
                 <div className="divider"></div>
-                <Skills />
+                <Skills className="yContainer" />
                 <div className="divider"></div>
-                <Projects />
+                <Projects className="yContainer" />
                 <div className="divider"></div>
-                <TimeLine />
+                <TimeLine className="yContainer" />
                 <div className="divider"></div>
-                <Footer />
+                <Footer className="yContainer" />
                 <div className="divider"></div>
                 <FooterContainer>Jin-Ah's portfolio</FooterContainer>
-            </div>
+            </FullPageWrapper>
         </>
     );
 };
 
 export default MyPortfolio;
+
+const FullPageWrapper = styled.div`
+    height: 95vh;
+    overflow-y: auto;
+    scroll-behavior: smooth;
+    scroll-snap-type: y mandatory;
+    font-family: "East Sea Dokdo", sans-serif;
+    ::-webkit-scrollbar {
+        width: 0;
+        background: transparent;
+    }
+`;
 
 const FooterContainer = styled.span`
     height: 7vh;
